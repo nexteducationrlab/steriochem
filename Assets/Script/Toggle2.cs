a426bfb449aa2112ddfe00948655b2561802134e
nexteducationrlab <39303321+nexteducationrlab@users.noreply.github.com>
2018-11-28 12:27:25 +0530

    Add files via upload

---
+++ Assets/Script/Toggle2.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toggle2 : MonoBehaviour {
+
+	public GameObject plane1;
+	public GameObject plane3;
+	public GameObject plane5;
+	public GameObject plane7;
+	public GameObject plane9;
+	public GameObject plane11;
+	public GameObject plane13;
+	public GameObject plane15;
+	public GameObject plane17;
+	public GameObject plane19;
+	public GameObject plane21;
+
+
+
+
+
+	public Slider slider;
+
+	public void Slider_Changed1(float newValue)
+	{
+		Vector3 pos = plane1.transform.position;
+		pos.x = newValue;
+		plane1.transform.position = pos;
+	}
+
+	public void Slider_Changed3(float newValue)
+	{
+		Vector3 pos = plane3.transform.position;
+		pos.x = newValue;
+		plane3.transform.position = pos;
+	}
+
+	public void Slider_Changed5(float newValue)
+	{
+		Vector3 pos = plane5.transform.position;
+		pos.x = newValue;
+		plane5.transform.position = pos;
+	}
+
+	public void Slider_Changed7(float newValue)
+	{
+		Vector3 pos = plane7.transform.position;
+		pos.x = newValue;
+		plane7.transform.position = pos;
+	}
+
+	public void Slider_Changed9(float newValue)
+	{
+		Vector3 pos = plane9.transform.position;
+		pos.x = newValue;
+		plane9.transform.position = pos;
+	}
+	public void Slider_Changed11(float newValue)
+	{
+		Vector3 pos = plane11.transform.position;
+		pos.x = newValue;
+		plane11.transform.position = pos;
+	}
+
+	public void Slider_Changed13(float newValue)
+	{
+		Vector3 pos = plane13.transform.position;
+		pos.x = newValue;
+		plane13.transform.position = pos;
+	}
+
+	public void Slider_Changed15(float newValue)
+	{
+		Vector3 pos = plane15.transform.position;
+		pos.x = newValue;
+		plane15.transform.position = pos;
+	}
+
+	public void Slider_Changed17(float newValue)
+	{
+		Vector3 pos = plane17.transform.position;
+		pos.x = newValue;
+		plane17.transform.position = pos;
+	}
+
+	public void Slider_Changed19(float newValue)
+	{
+		Vector3 pos = plane19.transform.position;
+		pos.x = newValue;
+		plane19.transform.position = pos;
+	}
+
+	public void Slider_Changed21(float newValue)
+	{
+		Vector3 pos = plane21.transform.position;
+		pos.x = newValue;
+		plane21.transform.position = pos;
+	}
+
+
+
+	public void Toggle_change1(bool newValue)
+	{
+		plane1.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change3(bool newValue)
+	{
+		plane3.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change5(bool newValue)
+	{
+		plane5.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change7(bool newValue)
+	{
+		plane7.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change9(bool newValue)
+	{
+		plane9.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change11(bool newValue)
+	{
+		plane11.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change13(bool newValue)
+	{
+		plane13.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change15(bool newValue)
+	{
+		plane15.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change17(bool newValue)
+	{
+		plane17.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change19(bool newValue)
+	{
+		plane19.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change21(bool newValue)
+	{
+		plane21.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+
+
+
+
+}
---
+++ Assets/Script/Toggle3.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Toggle3 : MonoBehaviour {
+
+	public GameObject plane2;
+	public GameObject plane4;
+	public GameObject plane6;
+	public GameObject plane8;
+	public GameObject plane10;
+	public GameObject plane12;
+	public GameObject plane14;
+	public GameObject plane16;
+	public GameObject plane18;
+	public GameObject plane20;
+	public GameObject plane22;
+
+
+
+
+	public Slider slider;
+
+	public void Slider_Changed2(float newValue)
+	{
+		Vector3 pos = plane2.transform.position;
+		pos.z = newValue;
+		plane2.transform.position = pos;
+	}
+
+	public void Slider_Changed4(float newValue)
+	{
+		Vector3 pos = plane4.transform.position;
+		pos.z = newValue;
+		plane4.transform.position = pos;
+	}
+
+	public void Slider_Changed6(float newValue)
+	{
+		Vector3 pos = plane6.transform.position;
+		pos.z = newValue;
+		plane6.transform.position = pos;
+	}
+
+	public void Slider_Changed8(float newValue)
+	{
+		Vector3 pos = plane8.transform.position;
+		pos.z = newValue;
+		plane8.transform.position = pos;
+	}
+
+	public void Slider_Changed10(float newValue)
+	{
+		Vector3 pos = plane10.transform.position;
+		pos.z = newValue;
+		plane10.transform.position = pos;
+	}
+	public void Slider_Changed12(float newValue)
+	{
+		Vector3 pos = plane12.transform.position;
+		pos.z = newValue;
+		plane12.transform.position = pos;
+	}
+
+	public void Slider_Changed14(float newValue)
+	{
+		Vector3 pos = plane14.transform.position;
+		pos.z = newValue;
+		plane14.transform.position = pos;
+	}
+
+	public void Slider_Changed16(float newValue)
+	{
+		Vector3 pos = plane16.transform.position;
+		pos.z = newValue;
+		plane16.transform.position = pos;
+	}
+
+	public void Slider_Changed18(float newValue)
+	{
+		Vector3 pos = plane18.transform.position;
+		pos.z = newValue;
+		plane18.transform.position = pos;
+	}
+
+	public void Slider_Changed20(float newValue)
+	{
+		Vector3 pos = plane20.transform.position;
+		pos.z = newValue;
+		plane20.transform.position = pos;
+	}
+
+	public void Slider_Changed22(float newValue)
+	{
+		Vector3 pos = plane22.transform.position;
+		pos.z = newValue;
+		plane22.transform.position = pos;
+	}
+
+
+
+	public void Toggle_change2(bool newValue)
+	{
+		plane2.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change4(bool newValue)
+	{
+		plane4.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change6(bool newValue)
+	{
+		plane6.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change8(bool newValue)
+	{
+		plane8.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change10(bool newValue)
+	{
+		plane10.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change12(bool newValue)
+	{
+		plane12.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change14(bool newValue)
+	{
+		plane14.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change16(bool newValue)
+	{
+		plane16.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change18(bool newValue)
+	{
+		plane18.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change20(bool newValue)
+	{
+		plane20.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+	public void Toggle_change22(bool newValue)
+	{
+		plane22.SetActive (newValue);
+		slider.interactable=(newValue);
+	}
+
+}
